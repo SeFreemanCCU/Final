caa1ca1937acde7aaa74a6cb8711718bae5f4838
Seth Freeman <sefreeman@students.ccu.edu>
2024-05-07 08:40:02 -0600

    Completed Program

---
+++ FinalApp/Program.cs
@@ -0,0 +1,15 @@
+﻿while (true)
+{
+    Console.WriteLine("Please input your birthday in the form mm/dd/yyyy.");
+    var response = Console.ReadLine();
+    if (DateTime.TryParse(response, out var birthday))
+    {
+        var days = (DateTime.Now - birthday).TotalDays;
+        Console.WriteLine($"You are {(int)days} days old.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please input a valid date.");
+    }
+}
